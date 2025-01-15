f9cf6ac9692ca2727bbcc67537e158f11323bd7e
Kim-Lan <me@kim-lan.com>
2025-01-14 17:09:14 -0800

    Fix for mobile-first design

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-teal-600 opacity-85 p-2 flex justify-center items-center gap-1.5">
+  <div class="bg-teal-600 p-2 flex justify-center items-center gap-1.5">
     <a href="https://github.com/Kim-Lan/photography-portfolio-template" target="_blank" class="flex">
       <font-awesome-icon :icon="['fab', 'github']" class="text-gray-50 w-5 h-5" />
     </a>
--- src/components/HomeContainer.vue
+++ src/components/HomeContainer.vue
@@ -3,18 +3,18 @@ import Button from './Button.vue'
 </script>
 
 <template>
-  <div class="w-full h-full p-6 max-sm:p-4 flex md:gap-4 max-sm:gap-2 max-sm:flex-col lg:justify-between md:justify-center max-sm:justify-end">
-    <div class="lg:w-1/4 max-sm:w-full">
+  <div class="w-full h-full p-4 md:p-6 flex gap-2 md:gap-4 flex-col md:flex-row justify-end md:justify-center lg:justify-between">
+    <div class="w-full md:w-0 lg:w-1/4">
     </div>
-    <div class="flex flex-col justify-end align-center lg:p-12 max-sm:p-0 lg:w-1/3 md:w-1/2 max-sm:w-full">
+    <div class="flex flex-col justify-end align-center p-0 lg:p-8 w-full md:w-1/2 lg:w-1/3">
       <a href="https://github.com/Kim-Lan" target="_blank">
         <Button append-icon="fa-solid fa-chevron-right" class="text-3xl p-4">Book Now</Button>
       </a>
     </div>
-    <div class="flex md:flex-col gap-2 lg:w-1/4 md:w-1/3 max-sm:w-full lg:justify-start md:justify-end max-sm:flex-wrap">
-      <Button prepend-icon="fa-solid fa-tree" class="text-xl px-4 py-2 max-sm:flex-1">Landscape</Button>
-      <Button prepend-icon="fa-solid fa-user" class="text-xl px-4 py-2 max-sm:flex-1">Portrait</Button>
-      <Button prepend-icon="fa-brands fa-square-instagram" class="text-xl px-4 py-2 max-sm:flex-1">Instagram</Button>
+    <div class="flex md:flex-col gap-2 w-full md:w-1/3 lg:w-1/4 justify-end lg:justify-start flex-wrap">
+      <Button prepend-icon="fa-solid fa-tree" class="text-xl px-4 py-2 flex-1 md:flex-none">Landscapes</Button>
+      <Button prepend-icon="fa-solid fa-user" class="text-xl px-4 py-2 flex-1 md:flex-none">Portraits</Button>
+      <Button prepend-icon="fa-brands fa-square-instagram" class="text-xl px-4 py-2 flex-1 md:flex-none">Instagram</Button>
     </div>
     
   </div>
